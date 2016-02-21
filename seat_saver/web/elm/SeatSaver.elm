dbf52705a90035c12f1cdbcaf209c88c62fae413
Cortland Walker <cortland1568@gmail.com>
2016-02-20 20:42:15 -0600

    Generated elm files, and created elm module that imports HTML Library.

---
+++ seat_saver/web/elm/SeatSaver.elm
@@ -0,0 +1,9 @@
+module SeatSaver where
+
+-- Imports HTML so that we can use its functions.
+import Html
+
+-- Every Elm application must have a main function that acts as its starting point.
+main =
+    -- Will result in the string being written out to the screen.
+    Html.text "Hello from Elm"
